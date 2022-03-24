673c55138eb52ea99f28f5357906cce540e21241
kimdaeyeon <alpioneer@gmail.com>
2022-03-24 21:13:55 +0900

    git test..

---
+++ src/components/Router.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { HashRouter as Router,Route,Routes} from "react-router-dom";
+import Header from "components/Header"
+
+
+
+
+const AppRouter = () => {
+
+    return(
+
+        <>
+
+        <Router>
+
+    <Routes>
+
+
+        
+   </Routes>
+        </Router>
+
+        </>
+    )
+
+    
+}
+
+export default AppRouter;
