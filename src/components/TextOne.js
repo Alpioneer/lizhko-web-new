021e6801aa595de90eebd6a3b8902f5196e13ed3
kimdaeyeon <alpioneer@gmail.com>
2022-04-27 17:56:09 +0900

    ClassName 상속으로 스타일 컴포넌트 재사용성 확장 가능

--- src/components/TextOne.js
+++ src/components/TextOne.js
@@ -1,12 +1,15 @@
 import React from "react";
-import styled from "styled-components";
 
 
 const TextOne = (props)=>{
+
+
     
-    return (<>
+    return (
+    <>
     <h1>{props.text}</h1>
-    </>)
+    </>
+    )
 
 }
 
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,11 +1,14 @@
 import React from "react";
 import styled from "styled-components";
+import ReactPlayer from "react-player";
+
+
 
 
 const Home =()=>{
 
     const Banner=styled.div`
-    width:100vw;
+    width:100%;
     height:600px;
     background:black;
     color:white;
@@ -14,12 +17,43 @@ const Home =()=>{
     text-align:center;
     align-items:center;
     line-height:30px;
+
     `
+
+    const VideoWrapper =styled.section`
+    display:flex;
+    width:100%;
+    `
+
+    const TestPlayer=styled(ReactPlayer)`
+    width=100%;
+height=600px;
+
+    `
+
+    const TestText=styled.span`
+    color:red;
+    font-size:20px;
+    `
+
+
+    /* https://codingbroker.tistory.com/18*/
     return(
 
         <>
        <Banner>Now is the time to think more about the meaning of design in business.<br/>
-기업의 비즈니스에서 디자인의 의미를 생각해 볼 adsfasdfas때입니다.</Banner>
+기업의 비즈니스에서 디자인의 의미를 생각해 볼 때입니다.</Banner>
+
+<VideoWrapper>
+
+<TestText>Now is the time to think more about the meaning of design in business.<br/>
+기업의 비즈니스에서 디자인의 의미를 생각해 볼 때입니다.</TestText>
+<TestPlayer
+          className='react-player'
+          url='https://www.youtube.com/watch?v=ysz5S6PUM-U'
+        />
+</VideoWrapper>
+
         </>
         
     )
