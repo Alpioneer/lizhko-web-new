c3aa18da5b860eb012fc0141a1d3c97732d17ca1
kimdaeyeon <alpioneer@gmail.com>
2022-04-27 16:28:34 +0900

    test 428

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -18,8 +18,8 @@ const Home =()=>{
     return(
 
         <>
-       <Banner>Now is the time to think more about the meaning of design in business.<br/>
-기업의 비즈니스에서 디자인의 의미를 생각해 볼 때입니다.</Banner>
+       <Banner>Now is the time to think more aadsfasfbout the meaning of design in business.<br/>
+기업의 비즈니스에서 디자인의 의미를 생각asdfasfas해 볼 때입니다.</Banner>
         </>
         
     )
