a84cb09f57f96a7f2744aa0529f37e6b10e90725
kimdaeyeon <alpioneer@gmail.com>
2022-05-03 22:43:24 +0900

    dummy is now technically working, buy i'm not sure wht it is done

--- src/routes/Interface-service/Task/UI-design.js
+++ src/routes/Interface-service/Task/UI-design.js
@@ -2,20 +2,8 @@ import WorkPost from "components/WorkPost";
 import React from "react";
 import styled from "styled-components";
 
+import carbom1 from "images/works/carbom/carbom1.png"
 
-/*image*/
-
-
-import carbom1 from 'images/works/carbom/carbom1.jpeg'
-import carbom2 from 'images/works/carbom/carbom2.jpeg'
-import carbom3 from 'images/works/carbom/carbom3.jpeg'
-import carbom4 from 'images/works/carbom/carbom4.jpeg'
-import hao1 from 'images/works/hao/hao1.jpeg'
-import hao2 from 'images/works/hao/hao2.jpeg'
-
-
-
-/*function*/
 
 
 
@@ -29,7 +17,16 @@ const UIdesign =()=>{
     
     `
 
-    const CarbomDummy=[carbom1,carbom2,carbom3,carbom4]
+    const dummy={
+
+        "images":[
+            "images/works/carbom/carbom1.png",
+            "images/works/carbom/carbom2.png",
+            "images/works/carbom/carbom3.png",
+            "images/works/carbom/carbom4.png",
+            carbom1
+        ]
+    }
 
 
     return(
@@ -37,7 +34,13 @@ const UIdesign =()=>{
         <>
         <Null>
 
-            <WorkPost image={CarbomDummy} title={`Carbom (IMS.connect)`} author={`Client : IMS.Mobility Director : Daeyeon Kim Design : Ordo`} desc={`IMS.Mobility Inc.와 2021년 4월부터 6월까지 진행한 Carbom 프로젝트입니다. 구축된 초기 프로토타입 베이스의 하이브리드 앱의 디자인 시스템 제작 및 , 마스터 레벨의 인터페이스를 생성하여, 다양한 페이지에서 생성 가능하게 끔, 컨셉 시안부터 상용화 시안까지의 그래픽 작업을 거친 후, 디자인 배포하였습니다. 명확한 정량적 기능의 사용을 작업 초점으로 두고, 고객사 측의 요구를 최대한 수용할 수 있도록, 상시 소통을 진행하였습니다.`}/>
+            <div>
+                {dummy.images.map(x=>{{
+                    return <img src={x}/>
+                }})}
+            </div>
+
+            <WorkPost title={`Carbom (IMS.connect)`} author={`Client : IMS.Mobility Director : Daeyeon Kim Design : Ordo`} desc={`IMS.Mobility Inc.와 2021년 4월부터 6월까지 진행한 Carbom 프로젝트입니다. 구축된 초기 프로토타입 베이스의 하이브리드 앱의 디자인 시스템 제작 및 , 마스터 레벨의 인터페이스를 생성하여, 다양한 페이지에서 생성 가능하게 끔, 컨셉 시안부터 상용화 시안까지의 그래픽 작업을 거친 후, 디자인 배포하였습니다. 명확한 정량적 기능의 사용을 작업 초점으로 두고, 고객사 측의 요구를 최대한 수용할 수 있도록, 상시 소통을 진행하였습니다.`}/>
             <WorkPost title={`Carbom (IMS.connect)`} author={`Client : IMS.Mobility Director : Daeyeon Kim Design : Ordo`} desc={`IMS.Mobility Inc.와 2021년 4월부터 6월까지 진행한 Carbom 프로젝트입니다. 구축된 초기 프로토타입 베이스의 하이브리드 앱의 디자인 시스템 제작 및 , 마스터 레벨의 인터페이스를 생성하여, 다양한 페이지에서 생성 가능하게 끔, 컨셉 시안부터 상용화 시안까지의 그래픽 작업을 거친 후, 디자인 배포하였습니다. 명확한 정량적 기능의 사용을 작업 초점으로 두고, 고객사 측의 요구를 최대한 수용할 수 있도록, 상시 소통을 진행하였습니다.`}/>
             <WorkPost title={`Carbom (IMS.connect)`} author={`Client : IMS.Mobility Director : Daeyeon Kim Design : Ordo`} desc={`IMS.Mobility Inc.와 2021년 4월부터 6월까지 진행한 Carbom 프로젝트입니다. 구축된 초기 프로토타입 베이스의 하이브리드 앱의 디자인 시스템 제작 및 , 마스터 레벨의 인터페이스를 생성하여, 다양한 페이지에서 생성 가능하게 끔, 컨셉 시안부터 상용화 시안까지의 그래픽 작업을 거친 후, 디자인 배포하였습니다. 명확한 정량적 기능의 사용을 작업 초점으로 두고, 고객사 측의 요구를 최대한 수용할 수 있도록, 상시 소통을 진행하였습니다.`}/>
             <WorkPost title={`Carbom (IMS.connect)`} author={`Client : IMS.Mobility Director : Daeyeon Kim Design : Ordo`} desc={`IMS.Mobility Inc.와 2021년 4월부터 6월까지 진행한 Carbom 프로젝트입니다. 구축된 초기 프로토타입 베이스의 하이브리드 앱의 디자인 시스템 제작 및 , 마스터 레벨의 인터페이스를 생성하여, 다양한 페이지에서 생성 가능하게 끔, 컨셉 시안부터 상용화 시안까지의 그래픽 작업을 거친 후, 디자인 배포하였습니다. 명확한 정량적 기능의 사용을 작업 초점으로 두고, 고객사 측의 요구를 최대한 수용할 수 있도록, 상시 소통을 진행하였습니다.`}/>
